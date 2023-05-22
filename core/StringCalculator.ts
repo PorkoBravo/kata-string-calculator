901c22963d78153e88730ba577ca5fe4fc452b12
Darío Beiró <dario.beiro@dxc.com>
2023-05-22 18:21:26 +0200

    Small refactors: extract methods, constants and rename

--- core/StringCalculator.ts
+++ core/StringCalculator.ts
@@ -1,11 +1,20 @@
 export function calculate(expression: string): number {
-    if(!!!expression) {
+    if (isFalsy(expression)) {
         return 0;
     }
 
-    const sum = expression.split(",").map( element => {
+    const sum = separateElementsOf(expression).map(element => {
         return parseInt(element);
-    }).reduce((a,b) => a+b)
-
+    }).reduce((a, b) => a + b)
     return sum
 }
+
+function separateElementsOf(expression: string): string[] {
+    const SEPARATOR = ","
+    return expression.split(SEPARATOR)
+}
+
+function isFalsy(expression: string): boolean {
+    return !!!expression;
+}
+
